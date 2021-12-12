cd5a5909595a385d054e3125bae710643f06802a
SinggitBram <bambadom@gmail.com>
2021-12-12 15:59:35 +0700

    added react intersection observer

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Button from "./Button";
 import "./InfoSection.css";
+import { useInView } from 'react-intersection-observer';
 
 const InfoSection = ({ heading, paragraphOne, paragraphTwo, buttonLabel, reverse, image }) => {
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0.2,
+  });
+  
   return (
-    <div className="infoSection">
+    <div className={inView ? "infoSection infoSection--zoom" : "infoSection"} ref={ref}>
       <div className="infoContainer">
         <div className="columnLeft" style={{ order: reverse ? "2" : "1" }}>
           <h1>{heading}</h1>
